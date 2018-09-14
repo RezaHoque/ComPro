04427de9634188b24267e1b47abf804a644fea30
Rashedul Hasan <pori468@gmail.com>
2018-09-14 21:49:08 +0200

    Few modifications

--- ComPro/ComPro/Controllers/EventController.cs
+++ ComPro/ComPro/Controllers/EventController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -19,11 +20,11 @@ namespace ComPro.Controllers
     {
         private readonly IEvent _eventManager;
         private readonly INoticeBoard _noticeBoardManager;
-        
 
-        public EventController(IEvent eventarg,INoticeBoard noticeboardManager)
+
+        public EventController(IEvent eventarg, INoticeBoard noticeboardManager)
         {
-           _eventManager = eventarg;
+            _eventManager = eventarg;
             _noticeBoardManager = noticeboardManager;
 
         }
@@ -60,12 +61,12 @@ namespace ComPro.Controllers
 
         }
         // GET: Event/Details/5
-        
+
         public ActionResult EventDetails(int id)
         {
 
             return PartialView("_EventDetails", _eventManager.Detail(id));
-           
+
         }
 
 
@@ -75,84 +76,94 @@ namespace ComPro.Controllers
             //return PartialView("_Create_PartialView");
             return View();
         }
-        
-        
+
+
+
+
         [HttpPost]
-        public ActionResult Create(EventModel eventModel,FormCollection frm)
+        public ActionResult Create(EventModel eventModel, FormCollection frm)
         {
-            if (ModelState.IsValid)
+
+            var invitees = new List<string>();
+            if (!string.IsNullOrEmpty(frm["invitees"]))
             {
-                var invitees = new List<string>();
-                if (!string.IsNullOrEmpty(frm["invitees"]))
-                {
-                    invitees = frm["invitees"].Split(',').ToList();
-                }
-
-                if (!string.IsNullOrEmpty(frm["startTime"]) && frm["startTime"]!= "Choose...")
-                {
-                    var timearr = frm["startTime"].Split(':');
-                    var time = new TimeSpan(int.Parse(timearr[0]), int.Parse(timearr[1]), 0);
-                    eventModel.Date = eventModel.Date.Add(time);
-                }
-                if (!string.IsNullOrEmpty(frm["endTime"]) && frm["endTime"] != "Choose...")
-                {
-                    var timearr = frm["endTime"].Split(':');
-                    var time = new TimeSpan(int.Parse(timearr[0]), int.Parse(timearr[1]), 0);
-                    eventModel.End = eventModel.End.Add(time);
-                }
-
-
-                var result = _eventManager.Create(eventModel,invitees);
-                if (result!=null)
-                {
-                    if (Request.Files.Count > 0)
-                    {
-                        var file = Request.Files[0];
-
-                        if (file != null && file.ContentLength > 0)
-                        {
-                            var fileName = Guid.NewGuid() + "_" + Path.GetFileName(file.FileName);
-                            var path = Path.Combine(Server.MapPath("~/Content/images/"), fileName);
-                            file.SaveAs(path);
-                            var image = new SiteImage
-                            {
-                                ImagePath = "/Content/images/" + fileName,
-                                Type = "Event",
-                                TypeId = result.EventId,
-                                UploadDate = DateTime.Now,
-                                UploaderId = result.CreatorId
-
-                            };
-                            _noticeBoardManager.SaveImage(image);
-                        }
-
-                        else
-                        {
-                            var image = new SiteImage
-                            {
-                                ImagePath = "/Content/images/Event/DefaultImage2.jpg",
-                                Type = "Event",
-                                TypeId = result.EventId,
-                                UploadDate = DateTime.Now,
-                                UploaderId = result.CreatorId
-
-                            };
-                            _noticeBoardManager.SaveImage(image);
-
-                        }
-                    }
-                    return RedirectToAction("Index");
-                }
-                    
-                //return Content(.ToString());
-
+                invitees = frm["invitees"].Split(',').ToList();
             }
+
+
+            eventModel.Date = DateTime.ParseExact(frm["Date"], "dd-MM-yyyy", CultureInfo.InvariantCulture); // String to datetime
+            eventModel.End = DateTime.ParseExact(frm["End"], "dd-MM-yyyy", CultureInfo.InvariantCulture); // String to datetime
+
+
+
+            if (!string.IsNullOrEmpty(frm["startTime"]) && frm["startTime"] != "Choose...")
+            {
+                var timearr = frm["startTime"].Split(':');
+                var time = new TimeSpan(int.Parse(timearr[0]), int.Parse(timearr[1]), 0);
+                eventModel.Date = eventModel.Date.Add(time);
+            }
+
+
+            if (!string.IsNullOrEmpty(frm["endTime"]) && frm["endTime"] != "Choose...")
+            {
+                var timearr = frm["endTime"].Split(':');
+                var time = new TimeSpan(int.Parse(timearr[0]), int.Parse(timearr[1]), 0);
+                eventModel.End = eventModel.End.Add(time);
+            }
+
+
+            var result = _eventManager.Create(eventModel, invitees);
+
+            if (result != null)
+            {
+                if (Request.Files.Count > 0)
+                {
+                    var file = Request.Files[0];
+
+                    if (file != null && file.ContentLength > 0)
+                    {
+                        var fileName = Guid.NewGuid() + "_" + Path.GetFileName(file.FileName);
+                        var path = Path.Combine(Server.MapPath("~/Content/images/"), fileName);
+                        file.SaveAs(path);
+                        var image = new SiteImage
+                        {
+                            ImagePath = "/Content/images/" + fileName,
+                            Type = "Event",
+                            TypeId = result.EventId,
+                            UploadDate = DateTime.Now,
+                            UploaderId = result.CreatorId
+
+                        };
+                        _noticeBoardManager.SaveImage(image);
+                    }
+
+                    else
+                    {
+                        var image = new SiteImage
+                        {
+                            ImagePath = "/Content/images/Event/DefaultImage2.jpg",
+                            Type = "Event",
+                            TypeId = result.EventId,
+                            UploadDate = DateTime.Now,
+                            UploaderId = result.CreatorId
+
+                        };
+                        _noticeBoardManager.SaveImage(image);
+
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+
+
+
+
             return View();
 
         }
 
-            
-        
+
+
 
 
         public ActionResult Members()
@@ -165,22 +176,22 @@ namespace ComPro.Controllers
         public ActionResult Members(EventModel eventModel)
         {
             // We must return to event Index as EventType.All
-           return View(eventModel);
+            return View(eventModel);
         }
 
-        public ActionResult ApproveEvent( int ? Id)
+        public ActionResult ApproveEvent(int? Id)
         {
-           bool result = _eventManager.ApproveEvent(Id.Value);
+            bool result = _eventManager.ApproveEvent(Id.Value);
             return Content(result.ToString());
-          
+
 
         }
 
 
-       // GET: Event/Edit/5
+        // GET: Event/Edit/5
         public ActionResult Edit(int id)
         {
-           
+
             var Result = _eventManager.GetEdit(id);
             if (Result == null)
             {
@@ -190,11 +201,11 @@ namespace ComPro.Controllers
 
             }
             return PartialView("_EditPartialView", Result);
-            
+
         }
 
         [HttpPost]
-         public ActionResult Edit(EventModel eventModel)
+        public ActionResult Edit(EventModel eventModel)
 
         {
             if (ModelState.IsValid)
@@ -214,10 +225,10 @@ namespace ComPro.Controllers
             }
             bool res = _eventManager.GetDelete(id.Value);
             return Content(res.ToString());
-            
+
         }
 
-        
+
 
         protected override void Dispose(bool disposing)
         {
@@ -226,12 +237,12 @@ namespace ComPro.Controllers
         }
 
         [HttpGet]
-       public ActionResult MemberResponse(int ID, string Response)
+        public ActionResult MemberResponse(int ID, string Response)
         {
-        
+
             bool res = _eventManager.MemberResponse(ID, Response);
-            return Content(res.ToString());           
-            
+            return Content(res.ToString());
+
         }
 
     }
